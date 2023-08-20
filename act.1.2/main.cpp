b5697fe57a0ac0df9aeff45fb2e6e7d06115a2f6
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-08-19 20:54:27 -0700

    ni idea, tengo sueño

--- act.1.2/main.cpp
+++ act.1.2/main.cpp
@@ -16,8 +16,9 @@
 using namespace std;
 
 /*
-Function to find the maximum money that can be grabbed of a row of coins without
-grabbing adjacent coins.
+Description:
+    Function to find the maximum money that can be grabbed of a row of coins without
+    grabbing adjacent coins.
 Complexity, where n is the number of coins:
     Time: O(n)
     Space: O(1)
@@ -32,20 +33,28 @@ int maxMoney(vector<int> &coins) {
     return dp[N];
 }
 
+/*
+Description:
+    Main function.
+Test Cases:
+    1.
+        Input:
+            0
+        Output:
+            0
+    2.
+        Input:
+            1
+            743
+        Output:
+            743
+        
+*/
 int main() {
-    // We have 4 test cases
-    // The fist is an empty row
-    // The second is a row with the following coins: 5, 1, 2, 10, 6, 2
-    // The third is a row with the following coins: 7, 3, 9, 10, 8, 6
-    // The fourth is a row with the following coins: 7, 9, 10, 9, 3, 5, 2
-    int cases = 4;
-    for (int i = 0; i < cases; i++) {
-        int n;
-        cin >> n;
-        vector<int> coins(n);
-        for (int j = 0; j < n; j++) {
-            cin >> coins[j];
-        }
-        cout << maxMoney(coins) << endl;
-    }
+    int n;
+    cin >> n;
+    vector<int> coins(n);
+    for (int i = 0; i < n; ++i)
+        cin >> coins[i];
+    cout << maxMoney(coins) << endl;
 }
