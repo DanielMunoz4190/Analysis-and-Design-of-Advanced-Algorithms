3183646322aedb9f6f95ac3f0c08b92e05c6a508
Carlos David Amezcua Canales <cdamezcua@outlook.com>
2023-08-19 20:19:18 -0700

    Refactoricé el codigo y no lo testee

--- act.1.2/main.cpp
+++ act.1.2/main.cpp
@@ -1,12 +1,12 @@
 /**
  * @file main.cpp
  * @author Juan Daniel Muñoz Dueñas A01641792 / Carlos David Amezcua Canalez A01641742
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-08-19
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include <iostream>
@@ -15,16 +15,21 @@
 
 using namespace std;
 
-// Function to find the maximum money that can be grabbed
-// Complexity O(N)
-int maxMoney(const vector<int> &coins) {
-    vector<int> dp(3, 0);
+/*
+Function to find the maximum money that can be grabbed of a row of coins without
+grabbing adjacent coins.
+Complexity, where n is the number of coins:
+    Time: O(n)
+    Space: O(1)
+*/
+int maxMoney(vector<int> &coins) {
+    const int N = 2; // Recurrence relation degree
+    vector<int> dp(N + 1, 0);
     for (int i = 0; i < coins.size(); ++i) {
-        dp[2] = max(dp[0] + coins[i], dp[1]);
-        dp[0] = dp[1];
-        dp[1] = dp[2];
+        rotate(dp.begin(), dp.begin() + 1, dp.end());
+        dp[N] = max(dp[N - 2] + coins[i], dp[N - 1]);
     }
-    return dp[2];
+    return dp[N];
 }
 
 int main() {
@@ -34,11 +39,11 @@ int main() {
     // The third is a row with the following coins: 7, 3, 9, 10, 8, 6
     // The fourth is a row with the following coins: 7, 9, 10, 9, 3, 5, 2
     int cases = 4;
-    for(int i = 0; i < cases; i++) {
+    for (int i = 0; i < cases; i++) {
         int n;
         cin >> n;
         vector<int> coins(n);
-        for(int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             cin >> coins[j];
         }
         cout << maxMoney(coins) << endl;
--- act1.1/main.cpp
+++ act1.1/main.cpp
@@ -1,12 +1,12 @@
 /**
  * @file main.cpp
  * @author Juan Daniel Muñoz Dueñas A01641792 / Carlos David Amezcua Canalez A01641742
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-08-19
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include <iostream>
@@ -14,24 +14,32 @@
 
 using namespace std;
 
-// Function to merge two halves of a subarray
-// Complexity O(N)
-void merge(vector<double>& arr, int l, int m, int r) {
+/*
+Function to merge two halves of a subarray.
+Complexity, where n is the number of elements in the subarray:
+    Time: O(n)
+    Space: O(n)
+*/
+void merge(vector<double> &arr, int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
 
     // Create temp arrays
     vector<double> L(n1), R(n2);
 
-    // Copy data to temp arrays L[] and R[]
-    // Complexity O(N)
+    /*
+    Copy data to temp arrays L[] and R[]
+    Complexity O(N)
+    */
     for (int i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays back into arr[l..r]
-    // Complexity O(N)
+    /*
+    Merge the temp arrays back into arr[l..r]
+    Complexity O(N)
+    */
     int i = 0;
     int j = 0;
     int k = l;
@@ -39,34 +47,41 @@ void merge(vector<double>& arr, int l, int m, int r) {
         if (L[i] >= R[j]) {
             arr[k] = L[i];
             i++;
-        } else {
+        }
+        else {
             arr[k] = R[j];
             j++;
         }
         k++;
     }
 
-    // Copy the remaining elements of L[], if there are any
-    // Complexity O(N)
+    /*
+    Copy the remaining elements of L[], if there are any
+    Complexity O(N)
+    */
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    // Copy the remaining elements of R[], if there are any
-    // Complexity O(N)
+    /* Copy the remaining elements of R[], if there are any
+    Complexity O(N)
+    */
     while (j < n2) {
         arr[k] = R[j];
         j++;
         k++;
     }
-
 }
 
-// Recursive function to sort an array using merge sort
-// Complexity O(N log N)
-void mergeSort(vector<double>& arr, int l, int r) {
+/*
+Recursive function to sort an array using merge sort
+Complexity, where n is the number of elements in the array:
+    Time: O(n log n)
+    Space: O(n)
+*/
+void mergeSort(vector<double> &arr, int l, int r) {
     if (l >= r)
         return;
     int m = l + (r - l) / 2;
@@ -83,20 +98,16 @@ int main() {
     // Array with 11 elements with repeated values
     // Array with 100 elements with random values
 
-    int casos = 5;
-    for(int i = 0; i < casos; i++) {
-        int n;
-        cin >> n;
-        vector<double> arr(n);
-        for(int j = 0; j < n; j++) {
-            cin >> arr[j];
-        }
-        mergeSort(arr, 0, n - 1);
-        for(int j = 0; j < n; j++) {
-            cout << arr[j] << " ";
-        }
-        cout << endl;
+    int n;
+    cin >> n;
+    vector<double> arr(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
     }
-
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
