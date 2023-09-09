31f170b84625af06362afb1371e24280580c84c4
DanielMunoz4190 <a01641792@itesm.mx>
2023-09-09 11:43:47 -0600

    function to read files

--- integrated_activity/main.cpp
+++ integrated_activity/main.cpp
@@ -1,4 +1,39 @@
-#include <vector>
+/**
+ * @file main.cpp
+ * @author Carlos David Amezcua Canales A01641742
+ * @author Juan Daniel Muñoz Dueñas A01641792
+ * @brief  
+ * @version 0.1
+ * @date 09-09-2023
+ */
+
 #include <iostream>
+#include <fstream>
+#include <vector>
 #include <string>
+#include <algorithm>
+#include <stdexcept>
+#include <iterator>
+#include <utility>
+#include <sstream>
+
+using namespace std;
+
+string readFile(const string &fileName) {
+    ifstream archivo(fileName);
+
+    if (!archivo.is_open()) 
+        throw runtime_error("Error opening file " + fileName);
+
+    stringstream buffer;
+    buffer << archivo.rdbuf(); 
+    archivo.close(); 
+
+    return buffer.str();
+}
+
+int main()
+{
+    return 0;
+}
 
